a9394eb77d027f79e8f504cb6ca4fa8ffdf5fc1d
Matteo Monti <matteo.monti@rain.vg>
2017-06-17 17:46:53 +0200

    Add progressive/exists.h
    Provided with a template scope,sets its value to
    true if the provided index of the scope has been
    implemented.
    
    We will use it to determine how many instances of a
    scope exist in a class.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,25 @@
 
 #include <iostream>
 
+#include "progressive/macros.h"
+#include "progressive/exists.h"
+
+class myclass
+{
+public:
+
+    template <size_t, bool> struct myscope;
+
+    template <bool dummy> struct myscope <0, dummy>
+    {
+        static constexpr bool exists = true;
+    };
+};
+
 int main()
 {
-    std :: cout << "Hello, world." << std :: endl;
+    std :: cout << progressive :: exists <myclass :: myscope, 0, 0> :: value << std :: endl;
+    std :: cout << progressive :: exists <myclass :: myscope, 1, 0> :: value << std :: endl;
 }
 
 #endif
---
+++ src/progressive/macros.h
@@ -0,0 +1,14 @@
+#if !defined(__forward__) && !defined(__drop__progressive__macros__h)
+#define __drop__progressive__macros__h
+
+// Macros
+
+#ifdef __COUNTER__
+#define __counter__ __COUNTER__
+#endif
+
+#define progressive_indirect(scope)
+
+#define progressive(scope) progressive_indirect(scope)
+
+#endif
