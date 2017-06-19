b34d86d6fe827579e18dd78b4f54b12781f4e163
Matteo Monti <matteo.monti@rain.vg>
2017-06-19 19:15:10 +0200

    Update bytewise/mask.h
    Adds a compress mechanism that:
     - Sorts the ranges in the mask
     - Merges together all the ranges that are consecutive
       and have swap = false
    
    Along with the output produced by the arithmetic
    scanner, we are now ready to build an arithmetic
    visitor.

--- src/bytewise/mask.h
+++ src/bytewise/mask.h
@@ -52,6 +52,81 @@ namespace bytewise
 
             typedef typename iterator <false, ranges...> :: type type;
         };
+
+        class compress
+        {
+        public: // REMOVE ME
+
+            // Service nested classes
+
+            template <typename, size_t> struct min;
+
+            template <size_t inf> struct min <mask <>, inf>
+            {
+                static constexpr size_t beg = -1;
+                static constexpr size_t end = -1;
+                static constexpr bool swap = false;
+            };
+
+            template <size_t rbeg, size_t rend, bool rswap, typename... tail, size_t inf> struct min <mask <range <rbeg, rend, rswap>, tail...>, inf>
+            {
+                static constexpr bool better = (rbeg >= inf && rbeg < min <mask <tail...>, inf> :: beg);
+
+                static constexpr size_t beg = better ? rbeg : min <mask <tail...>, inf> :: beg;
+                static constexpr size_t end = better ? rend : min <mask <tail...>, inf> :: end;
+                static constexpr bool swap = better ? rswap : min <mask <tail...>, inf> :: swap;
+            };
+
+            template <size_t, bool> struct sort;
+
+            template <bool dummy> struct sort <0, dummy>
+            {
+                static constexpr size_t beg = min <mask <ranges...>, 0> :: beg;
+                static constexpr size_t end = min <mask <ranges...>, 0> :: end;
+                static constexpr bool swap = min <mask <ranges...>, 0> :: swap;
+
+                typedef mask <range <beg, end, swap>> type;
+            };
+
+            template <size_t index, bool dummy> struct sort
+            {
+                static constexpr size_t inf = sort <index - 1, false> :: beg + 1;
+
+                static constexpr size_t beg = min <mask <ranges...>, inf> :: beg;
+                static constexpr size_t end = min <mask <ranges...>, inf> :: end;
+                static constexpr bool swap = min <mask <ranges...>, inf> :: swap;
+
+                typedef typename sort <index - 1, false> :: type :: template append <mask <range <beg, end, swap>>> :: type type;
+            };
+
+            template <typename> struct merge;
+
+            template <size_t beg, size_t end, bool swap> struct merge <mask <range <beg, end, swap>>>
+            {
+                typedef mask <range <beg, end, swap>> type;
+            };
+
+            template <size_t abeg, size_t aend, bool aswap, size_t bbeg, size_t bend, size_t bswap, typename... tail> struct merge <mask <range <abeg, aend, aswap>, range <bbeg, bend, bswap>, tail...>>
+            {
+                typedef typename std :: conditional
+                <
+                    (!aswap && !bswap && aend == bbeg),
+                    typename merge <mask <range <abeg, bend, false>, tail...>> :: type,
+                    typename mask <range <abeg, aend, aswap>> :: template append <typename merge <mask <range <bbeg, bend, bswap>, tail...>> :: type> :: type
+                > :: type type;
+            };
+
+        public:
+
+            // Typedefs
+
+            typedef typename std :: conditional
+            <
+                (sizeof...(ranges) > 0),
+                typename merge <typename sort <sizeof...(ranges) - 1, false> :: type> :: type,
+                mask <>
+            > :: type type;
+        };
     };
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,29 @@
 #include <iostream>
 #include <type_traits>
 
-#include "bytewise/macros.h"
-#include "bytewise/scanners/arithmetic.h"
-
-class myotherclass
-{
-    // Self
-
-    typedef myotherclass self;
-
-    // Members
-
-    int a;
-    int b;
-
-    // Bytewise
-
-    bytewise(a);
-    bytewise(b);
-};
-
-class myclass
-{
-    // Self
-
-    typedef myclass self;
-
-    // Members
-
-    int x[4];
-    double y;
-    myotherclass m;
-    char q[16];
-
-    // Bytewise
-
-    bytewise(x);
-    bytewise(y);
-    bytewise(m);
-    bytewise(q);
-};
+#include "bytewise/mask.h"
 
 int main()
 {
     using namespace bytewise;
 
+    typedef mask
+    <
+        range <7, 8, true>,
+        range <3, 4, false>,
+        range <14, 16, false>,
+        range <16, 17, true>,
+        range <11, 12, false>,
+        range <2, 3, false>,
+        range <4, 5, true>,
+        range <8, 9, false>,
+        range <12, 14, false>
+    > :: compress :: type type;
+
     std :: cout << std :: is_same
     <
-        scanners :: arithmetic <myclass> :: type,
-        mask <range <0, 4, true>, range <4, 8, true>, range <8, 12, true>, range <12, 16, true>, range <16, 24, true>, range <24, 28, true>, range <28, 32, true>, range <32, 48, false>>
+        type,
+        mask <range <2, 4, false>, range <4, 5, true>, range <7, 8, true>, range <8, 9, false>, range <11, 16, false>, range <16, 17, true>>
     > :: value << std :: endl;
 }
 
