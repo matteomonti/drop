3bf2df988a7d27242266e505e58b42854682fa47
Matteo Monti <matteo.monti@rain.vg>
2017-06-28 20:49:34 +0200

    Add network/address
    Simple IPv4 address class. Works nicely, correctly
    resolves addresses and throws exceptions when needed.

--- src/main.cpp
+++ src/main.cpp
@@ -5,47 +5,12 @@
 #include <string>
 #include <iomanip>
 
-#include "bytewise/macros.h"
-#include "bytewise/serialize.hpp"
-#include "bytewise/deserialize.hpp"
-
-class myclass
-{
-public:
-
-    // Self
-
-    typedef myclass self;
-
-    // Members
-
-    int x;
-
-    // Bytewise
-
-    bytewise(x, endianess :: local);
-};
+#include "network/address.h"
 
 int main()
 {
-    myclass myobj;
-    myobj.x = 4;
-
-    auto bytes = bytewise :: serialize(myobj);
-
-    for(size_t i = 0; i < bytes.size(); i++)
-    {
-        std :: cout << std :: setw(5) << (unsigned int) (unsigned char) bytes[i];
-
-        if(i % 16 == 15)
-            std :: cout << std :: endl;
-    }
-
-    std :: cout << std :: endl;
-
-    myclass myotherobj = bytewise :: deserialize <myclass> (bytes);
-
-    std :: cout << myotherobj.x << std :: endl;
+    network :: address my_address("google.com", 80);
+    std :: cout << (my_address == network :: address("google.com", 80)) << std :: endl;
 }
 
 #endif
