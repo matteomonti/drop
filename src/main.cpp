831ca993a564155b6d881db15a210db66ddba930
Matteo Monti <matteo.monti@rain.vg>
2017-06-28 14:22:13 +0200

    Add bytewise/options.h
    Basic scaffolding for passing options to the bytewise
    marker. Implements a mechanism to express preferences
    for target endianess (which we will use, e.g., to
    serialize objects that already have network endianess,
    like addresses).

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 
 #include <iostream>
 
+#include "bytewise/options.h"
+
 int main()
 {
+    std :: cout << "Little: " << bytewise :: endianess :: little << std :: endl;
+    std :: cout << "Big: " << bytewise :: endianess :: big << std :: endl;
+    std :: cout << "Network: " << bytewise :: endianess :: network << std :: endl;
+    std :: cout << "Local: " << bytewise :: endianess :: local << std :: endl;
+
+    std :: cout << bytewise :: options :: pack <bytewise :: options :: endianess :: big> :: endianess << std :: endl;
 }
 
 #endif
