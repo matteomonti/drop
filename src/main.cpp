0171c6d4f9f84c443bf4c1e53ccf70f359b8e7c8
Matteo Monti <matteo.monti@rain.vg>
2017-06-30 13:27:52 +0200

    Add utils/template/all.h
    Implements recursion on arbitrary template type, to
    make a simultaneous check against a type parameter
    pack.

--- src/main.cpp
+++ src/main.cpp
@@ -5,62 +5,12 @@
 #include <string>
 #include <iomanip>
 
-#include "data/variant.hpp"
-
-class unmovable
-{
-public:
-
-    unmovable() = default;
-    unmovable(const unmovable &) = default;
-    unmovable(unmovable &&) = delete;
-
-    int x;
-
-    void operator = (int x)
-    {
-        this->x = x;
-    };
-
-    operator int()
-    {
-        return x;
-    }
-};
-
-typedef data :: variant <int, double, unmovable> my_variant_type;
-
-my_variant_type f()
-{
-    my_variant_type my_variant = my_variant_type :: construct <int> ();
-
-    my_variant.visit([](auto && value)
-    {
-        value = 12;
-    });
-
-    return my_variant;
-}
+#include "utils/template/all.h"
+#include "utils/misc/copiable.h"
 
 int main()
 {
-    my_variant_type my_variant = f();
-    my_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
-
-    my_variant_type my_other_variant = std :: move(my_variant);
-    my_other_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
-
-    my_variant_type yet_another_variant = my_other_variant;
-    yet_another_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
+    std :: cout << utils :: all <std :: is_copy_constructible, int, double, char, int, utils :: copiable <false>> :: value << std :: endl;
 }
 
 #endif
