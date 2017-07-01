fa25d00ecbe6028da9c154cc1f7165a2dca78fba
Matteo Monti <matteo.monti@rain.vg>
2017-07-01 19:55:56 +0200

    Add utils/template/is_callable.h
    Sfinae-based struct that tells if a functor is
    callable with a given argument. Still to be extended
    to multiple arguments.
    
    Offers two values: value just says if it is callable
    with that value. Direct says if it is callable
    without any cast, i.e., the functor explicitly
    accepts that value.

--- src/main.cpp
+++ src/main.cpp
@@ -5,100 +5,16 @@
 #include <string>
 #include <iomanip>
 
-#include "data/variant.hpp"
-
-class unmovable
-{
-public:
-
-    unmovable() = default;
-    unmovable(const unmovable &) = default;
-    unmovable(unmovable &&) = delete;
-
-    unmovable & operator = (const unmovable &) = default;
-    unmovable & operator = (unmovable &&) = delete;
-
-    int x;
-
-    void operator = (int x)
-    {
-        this->x = x;
-    };
-
-    operator int()
-    {
-        return x;
-    }
-};
-
-typedef data :: variant <int, double, unmovable> my_variant_type;
-
-my_variant_type f()
-{
-    my_variant_type my_variant = my_variant_type :: construct <int> ();
-
-    my_variant.visit([](auto && value)
-    {
-        value = 12;
-    });
-
-    return my_variant;
-}
-
-my_variant_type g()
-{
-    return my_variant_type :: construct <int> (44);
-}
-
-my_variant_type h()
-{
-    return my_variant_type :: construct <double> (5.59);
-}
+#include "utils/template/is_callable.h"
 
 int main()
 {
-    my_variant_type my_variant = f();
-    my_variant.visit([](auto && value)
+    auto lambda = [](const auto &)
     {
-        std :: cout << value << std :: endl;
-    });
+    };
 
-    my_variant_type my_other_variant = std :: move(my_variant);
-    my_other_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
-
-    my_variant_type yet_another_variant = my_other_variant;
-    yet_another_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
-
-    my_other_variant = g();
-    yet_another_variant = my_other_variant;
-
-    my_other_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
-
-    yet_another_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
-
-    yet_another_variant = my_other_variant = h();
-
-    my_other_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
-
-    yet_another_variant.visit([](auto && value)
-    {
-        std :: cout << value << std :: endl;
-    });
+    std :: cout << utils :: is_callable <decltype(lambda), const int> :: direct << std :: endl;
+    std :: cout << utils :: is_callable <decltype(lambda), const int> :: value << std :: endl;
 }
 
 #endif
---
+++ src/utils/template/is_callable.h
@@ -0,0 +1,76 @@
+// Forward declarations
+
+namespace utils
+{
+    template <typename, typename...> class is_callable;
+};
+
+#if !defined(__forward__) && !defined(__drop__utils__template__is_callable__h)
+#define __drop__utils__template__is_callable__h
+
+// Libraries
+
+#include <type_traits>
+#include <stdint.h>
+
+namespace utils
+{
+    template <typename functor, typename arg> class is_callable <functor, arg>
+    {
+    public: // REMOVE ME
+
+        // Service nested classes
+
+        typedef std :: remove_const_t <std :: remove_reference_t <arg>> clean;
+        static constexpr bool const_arg = std :: is_const <std :: remove_reference_t <arg>> :: value;
+
+        struct copy_sfinae
+        {
+            template <typename ftype, void (ftype :: *)(clean)> struct mhelper {};
+            template <typename ftype, void (ftype :: *)(clean) const> struct chelper {};
+
+            template <typename ftype> static uint8_t sfinae(...);
+            template <typename ftype> static uint32_t sfinae(mhelper <ftype, &ftype :: operator ()> *);
+            template <typename ftype> static uint32_t sfinae(chelper <ftype, &ftype :: operator ()> *);
+
+            static constexpr bool value = std :: is_same <decltype(sfinae <functor> (0)), uint32_t> :: value;
+        };
+
+        struct const_reference_sfinae
+        {
+            template <typename ftype, void (ftype :: *)(const clean &)> struct mhelper {};
+            template <typename ftype, void (ftype :: *)(const clean &) const> struct chelper {};
+
+            template <typename ftype> static uint8_t sfinae(...);
+            template <typename ftype> static uint32_t sfinae(mhelper <ftype, &ftype :: operator ()> *);
+            template <typename ftype> static uint32_t sfinae(chelper <ftype, &ftype :: operator ()> *);
+
+            static constexpr bool value = std :: is_same <decltype(sfinae <functor> (0)), uint32_t> :: value;
+        };
+
+        struct reference_sfinae
+        {
+            template <typename ftype, void (ftype :: *)(clean &)> struct mhelper {};
+            template <typename ftype, void (ftype :: *)(clean &) const> struct chelper {};
+
+            template <typename ftype> static uint8_t sfinae(...);
+            template <typename ftype> static uint32_t sfinae(mhelper <ftype, &ftype :: operator ()> *);
+            template <typename ftype> static uint32_t sfinae(chelper <ftype, &ftype :: operator ()> *);
+
+            static constexpr bool value = std :: is_same <decltype(sfinae <functor> (0)), uint32_t> :: value;
+        };
+
+        struct callable_sfinae
+        {
+            template <typename ftype> static uint8_t sfinae(...);
+            template <typename ftype> static uint32_t sfinae(std :: result_of_t <ftype(arg)> *);
+
+            static constexpr bool value = std :: is_same <decltype(sfinae <functor> (0)), uint32_t> :: value;
+        };
+
+        static constexpr bool direct = callable_sfinae :: value && (copy_sfinae :: value || const_reference_sfinae :: value || (reference_sfinae :: value && !const_arg));
+        static constexpr bool value = callable_sfinae :: value;
+    };
+};
+
+#endif
