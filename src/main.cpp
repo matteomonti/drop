6a4d45e7e43cf01d2bb2acf998de8cf221b97c08
Matteo Monti <matteo.monti@rain.vg>
2017-07-04 15:08:28 +0200

    Add network/acceptors/tcp
    First sketch of a direct tcp acceptor.

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,12 @@
 #include <string>
 #include <iomanip>
 
-#include "network/connection.hpp"
+#include "network/acceptors/tcp.h"
 
 int main()
 {
-    network :: sockets :: tcp my_tcp_socket;
-    my_tcp_socket.bind(1234);
-    my_tcp_socket.listen();
-    network :: sockets :: tcp my_inbound_connection = my_tcp_socket.accept();
-
-    network :: connection my_connection(my_inbound_connection);
+    network :: acceptors :: tcp :: sync my_acceptor(1235);
+    network :: connection my_connection = my_acceptor.accept();
 }
 
 #endif
