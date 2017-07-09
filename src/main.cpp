13e2189fead5c41934e8914d810b313a49c65ea5
Matteo Monti <matteo.monti@rain.vg>
2017-07-09 02:31:01 +0200

    Main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,8 @@
 #include <iostream>
 #include <math.h>
 
-#include "async/async.h"
-
-promise <int> my_int_promise;
-int my_int;
-
-promise <int> int_tomorrow()
-{
-    async
-    (
-        std :: cout << "First run" << std :: endl;
-        await(my_int) = my_int_promise;
-        std :: cout << "Second run" << std :: endl;
-        return context.resolve(22);
-    );
-}
-
 int main()
 {
-    int_tomorrow().then([](const int & value)
-    {
-        std :: cout << "Context resolved with " << value << std :: endl;
-    });
-
-    my_int_promise.resolve(55);
 }
 
 #endif
