bbb71de48400981b9c93a1a4e5f812f7606df683
Matteo Monti <matteo.monti@rain.vg>
2017-07-14 17:56:28 +0200

    Main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,8 @@
 
 #include <iostream>
 
-#include "network/pool/queue.hpp"
-#include "network/sockets/tcp.h"
-
 int main()
 {
-    network :: queue my_queue;
-    network :: sockets :: tcp my_socket;
-
-    std :: cout << "My socket descriptor: " << my_socket.descriptor() << std :: endl;
-
-    my_socket.block(false);
-    my_queue.add <network :: queue :: write> (my_socket.descriptor());
-    my_queue.add <network :: queue :: read> (my_socket.descriptor());
-
-    my_socket.connect({"google.com", 80});
-
-    size_t events = my_queue.select();
-    std :: cout << "Events (" << events << "):" << std :: endl;
-
-    for(size_t i = 0; i < events; i++)
-        std :: cout << my_queue[i].descriptor() << " (" << (my_queue[i].type() == network :: queue :: write ? "write" : "read") << ")" << std :: endl;
 }
 
 #endif
