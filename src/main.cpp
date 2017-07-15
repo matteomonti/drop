4d2911164430fd2efbfa353443ff95c24e822a85
Matteo Monti <matteo.monti@rain.vg>
2017-07-15 22:31:00 +0200

    Main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,8 @@
 
 #include <iostream>
 
-#include "data/hashtable.hpp"
-
 int main()
 {
-    data :: hashtable <int, int> x;
-
-    for(size_t i = 0; i < 1048576; i++)
-        x.add(i, i * 3 + 1);
-
-    for(size_t i = 0; i < 524288; i++)
-        x.remove(rand() % 1048576);
-
-    for(size_t i = 0; i < 1048576; i++)
-    {
-        data :: optional <int> value = x[i];
-        if(value)
-            std :: cout << *value << std :: endl;
-        else
-            std :: cout << "(empty)" << std :: endl;
-    }
 }
 
 #endif
