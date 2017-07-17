9fcbd698e3173876019d23ba640bb7a778431ed9
Matteo Monti <matteo.monti@rain.vg>
2017-07-17 17:51:34 +0200

    Main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,8 @@
 #include <unistd.h>
 #include <thread>
 
-#include "data/heap.hpp"
-
 int main()
 {
-    data :: heap <size_t> my_heap;
-
-    for(size_t i = 0; i < 1048576; i++)
-        my_heap.push(rand());
-
-    for(size_t i = 0; i < 1048576; i++)
-        std :: cout << my_heap.pop() << std :: endl;
 }
 
 #endif
