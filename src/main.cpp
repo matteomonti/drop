e36d2dbc8001097e629af008b705bf998f3bc28e
Matteo Monti <matteo.monti@rain.vg>
2017-07-17 18:30:43 +0200

    Main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,8 @@
 #include <unistd.h>
 #include <thread>
 
-#include "chrono/timelock.hpp"
-
 int main()
 {
-    chrono :: timelock <const char *> my_timelock;
-
-    my_timelock.push("Ladies and gentleman...", microtime() + 5.e6);
-    my_timelock.push("This is the story...", microtime() + 8.e6);
-    my_timelock.push("of", microtime() + 12.e6);
-    my_timelock.push("my", microtime() + 13.e6);
-    my_timelock.push("life.", microtime() + 14.e6);
-
-    while(true)
-    {
-        data :: optional <const char *> value = my_timelock.pop();
-
-        if(value)
-            std :: cout << *value << std :: endl;
-    }
 }
 
 #endif
