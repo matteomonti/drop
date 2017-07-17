a83275d9ae8e175be38f1b4f2f630b78ca1af3a5
Matteo Monti <matteo.monti@rain.vg>
2017-07-17 18:14:52 +0200

    Add chrono/timelock
    Basic timelock implementation. Uses heaps to manage
    a set of value / expiry associations. Elements are
    securely yielded by order of expiry, only after
    they are expired.
    
    This is *not* inherently thread safe.

---
+++ src/chrono/timelock.hpp
@@ -0,0 +1,42 @@
+#ifndef __drop__chrono__timelock__hpp
+#define __drop__chrono__timelock__hpp
+
+// Includes
+
+#include "timelock.h"
+
+namespace chrono
+{
+    // entry
+
+    // Operators
+
+    template <typename type> bool timelock <type> :: entry :: operator < (const entry & rho) const
+    {
+        return this->expiry < rho.expiry;
+    }
+
+    template <typename type> bool timelock <type> :: entry :: operator > (const entry & rho) const
+    {
+        return this->expiry > rho.expiry;
+    }
+
+    // timelock
+
+    // Methods
+
+    template <typename type> void timelock <type> :: push(const type & value, const microtimestamp & expiry)
+    {
+        this->_entries.push(entry{.value = value, .expiry = expiry});
+    }
+
+    template <typename type> data :: optional <type> timelock <type> :: pop()
+    {
+        if(this->_entries.size() && this->_entries.peek().expiry < microtime())
+            return this->_entries.pop().value;
+        else
+            return data :: null;
+    }
+};
+
+#endif
