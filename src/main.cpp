88415e789492bacacd3a53876a43d62f54e32660
Matteo Monti <matteo.monti@rain.vg>
2017-07-17 19:07:04 +0200

    Update connectors/tcp
    Async tcp connector now handles exceptions.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,31 @@
 #include <unistd.h>
 #include <thread>
 
+#include "network/connectors/tcp.h"
+
 int main()
 {
+    network :: connectors :: tcp :: async my_connector;
+
+    my_connector.connect({"google.it", 79}).then([](const network :: connection & connection)
+    {
+        std :: cout << "Connection successful" << std :: endl;
+    }).except([](const std :: exception_ptr & exception)
+    {
+        try
+        {
+            std :: rethrow_exception(exception);
+        }
+        catch(const std :: exception & exception)
+        {
+            std :: cout << exception.what() << std :: endl;
+        }
+    });
+
+    usleep(rand() % 100000);
+
+    while(true)
+        usleep(1.e6);
 }
 
 #endif
--- src/network/connectors/tcp.h
+++ src/network/connectors/tcp.h
@@ -25,6 +25,7 @@ namespace network
 #include "network/pool/queue.hpp"
 #include "async/promise.hpp"
 #include "thread/channel.hpp"
+#include "chrono/timelock.hpp"
 
 namespace network :: connectors
 {
@@ -45,12 +46,29 @@ namespace network :: connectors
 
         class async
         {
+        public:
+
+            struct settings
+            {
+                static constexpr microtimestamp timeout = 10e6;
+                static constexpr microtimestamp interval = 1e6;
+            };
+
+        private:
+
             // Service nested classes
 
             struct request
             {
                 sockets :: tcp socket;
                 promise <connection> promise;
+                size_t version;
+            };
+
+            struct timeout
+            {
+                int descriptor;
+                size_t version;
             };
 
             // Members
@@ -58,8 +76,11 @@ namespace network :: connectors
             queue _queue;
 
             volatile bool _alive;
+
             thread :: channel <request> _new;
             data :: hashtable <int, request> _pending;
+
+            chrono :: timelock <timeout> _timeouts;
 
             struct
             {
@@ -90,6 +111,10 @@ namespace network :: connectors
 
             void run();
             void wake();
+
+            // Static members
+
+            static size_t version;
         };
     };
 };
