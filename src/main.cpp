79581e705998aa23f4bce5f202be60320763b0e6
Matteo Monti <matteo.monti@rain.vg>
2017-07-19 15:32:04 +0200

    Main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,75 +4,8 @@
 #include <unistd.h>
 #include <thread>
 
-#include "network/acceptors/tcp.h"
-#include "network/connectors/tcp.h"
-#include "network/pool/pool.hpp"
-
-void server()
-{
-    network :: pool my_pool;
-
-    network :: acceptors :: tcp :: sync my_acceptor(1234);
-    network :: connection my_connection = my_acceptor.accept();
-
-    network :: pool :: connection my_pool_connection = my_pool.bind(my_connection);
-    my_pool_connection.receive <int> ().then([=](const int & value)
-    {
-        std :: cout << "Received " << value << std :: endl;
-        return my_pool_connection.receive <bytewise :: buffer> ();
-    }).then([=](const bytewise :: buffer & value)
-    {
-        std :: cout << "Received " << value << std :: endl;
-        // return my_pool_connection.send(99);
-    })/*.then([=]()
-    {
-        std :: cout << "Sent number" << std :: endl;
-        return my_pool_connection.send(bytewise :: buffer("Hello to you my friend!"));
-    }).then([=]()
-    {
-        std :: cout << "Sent message" << std :: endl;
-    })*/.except([](const std :: exception_ptr & exception)
-    {
-        try
-        {
-            std :: rethrow_exception(exception);
-        }
-        catch(const std :: exception & exception)
-        {
-            std :: cout << "Exception: " << exception.what() << std :: endl;
-        }
-    });
-
-    while(true)
-        usleep(1.e6);
-}
-
-void client()
-{
-    network :: connection my_connection = network :: connectors :: tcp :: sync :: connect({"localhost", 1234});
-    my_connection.send(55);
-    /*my_connection.send(bytewise :: buffer("Hello there!"));
-
-    {
-        int value = my_connection.receive <int> ();
-        std :: cout << "Blockingly received " << value << std :: endl;
-    }
-
-    {
-        bytewise :: buffer value = my_connection.receive <bytewise :: buffer> ();
-        std :: cout << "Blockingly received " << value << std :: endl;
-    }*/
-
-    while(true)
-        usleep(1.e6);
-}
-
 int main()
 {
-    std :: thread server_thread(server);
-    client();
-
-    server_thread.join();
 }
 
 #endif
