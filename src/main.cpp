d80a90a2d9c2e0710efaf079fcbc5fb9355fe938
Matteo Monti <matteo.monti@rain.vg>
2017-07-19 16:15:41 +0200

    Update network/acceptors/tcp
    Implements full async tcp acceptor.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,38 @@
 #include <unistd.h>
 #include <thread>
 
+#include "network/connectors/tcp.h"
+#include "network/acceptors/tcp.hpp"
+
 int main()
 {
+    network :: acceptors :: tcp :: async my_acceptor(1234);
+
+    my_acceptor.on <network :: connection> ([](const network :: connection & my_connection)
+    {
+        my_connection.send(bytewise :: buffer("Hello World!"));
+    });
+
+    my_acceptor.on <network :: connection> ([](const network :: connection & my_connection)
+    {
+        my_connection.send(bytewise :: buffer("Hello Mars!"));
+    });
+
+    network :: connection my_connection = network :: connectors :: tcp :: sync :: connect({"127.0.0.1", 1234});
+
+    std :: cout << my_connection.receive <bytewise :: buffer> () << std :: endl;
+    std :: cout << my_connection.receive <bytewise :: buffer> () << std :: endl;
+
+    my_acceptor.clear();
+
+    my_acceptor.on <network :: connection> ([](const network :: connection & my_connection)
+    {
+        my_connection.send(bytewise :: buffer("Hello Venus!"));
+    });
+
+    network :: connection my_other_connection = network :: connectors :: tcp :: sync :: connect({"127.0.0.1", 1234});
+
+    std :: cout << my_other_connection.receive <bytewise :: buffer> () << std :: endl;
 }
 
 #endif
--- src/network/acceptors/tcp.h
+++ src/network/acceptors/tcp.h
@@ -14,11 +14,16 @@ namespace network
 // Libraries
 
 #include <stdint.h>
+#include <type_traits>
+#include <thread>
+#include <mutex>
 
 // Includes
 
 #include "network/sockets/tcp.h"
 #include "network/connection.hpp"
+#include "utils/template/is_callable.h"
+#include "network/connectors/tcp.h"
 
 namespace network :: acceptors
 {
@@ -35,7 +40,7 @@ namespace network :: acceptors
             sockets :: tcp _socket;
 
         public:
-            
+
             // Constructors
 
             sync(const uint16_t &);
@@ -43,6 +48,85 @@ namespace network :: acceptors
             // Methods
 
             connection accept();
+        };
+
+        class async
+        {
+        public:
+
+            // Settings
+
+            struct settings
+            {
+                static constexpr size_t callbacks = 16;
+            };
+
+        private:
+
+            // Service nested classes
+
+            class callback_base
+            {
+            public:
+
+                // Destructor
+
+                virtual ~callback_base();
+
+                // Methods
+
+                virtual void run(const connection &) = 0;
+            };
+
+            template <typename lambda> class callback : public callback_base
+            {
+                // Members
+
+                lambda _callback;
+
+            public:
+
+                // Constructors
+
+                callback(const lambda &);
+
+                // Methods
+
+                void run(const connection &);
+            };
+
+            // Members
+
+            sync _acceptor;
+            uint16_t _port;
+            volatile bool _alive;
+
+            callback_base * _callbacks[settings :: callbacks];
+
+            std :: mutex _mutex;
+            std :: thread _thread;
+
+        public:
+
+            // Constructors
+
+            async(const uint16_t &);
+
+            // Destructor
+
+            ~async();
+
+            // Methods
+
+            template <typename etype, typename lambda, typename std :: enable_if_t <std :: is_same <etype, connection> :: value && utils :: is_callable <lambda, const connection &> :: value> * = nullptr> void on(const lambda &);
+            void clear();
+
+        private:
+
+            // Private methods
+
+            void run();
+            void wake();
         };
     };
 };
