56d9f2c21f85676345bdaf91e7952b736d45619d
Matteo Monti <matteo.monti@rain.vg>
2017-07-20 14:03:52 +0200

    Add thread/semaphore
    Adds fully functional semaphore, including timeout
    on wait. Works only on OSX.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,36 @@
 #include <unistd.h>
 #include <thread>
 
+#include "thread/semaphore.h"
+
+thread :: semaphore sema(1), semb;
+
+void a()
+{
+    while(true)
+    {
+        sema.wait();
+        std :: cout << "Hello ";
+        semb.post();
+    }
+}
+
+void b()
+{
+    while(true)
+    {
+        semb.wait();
+        std :: cout << "World!" << std :: endl;
+        sema.post();
+    }
+}
+
 int main()
 {
+    std :: thread ta(a), tb(b);
+
+    ta.join();
+    tb.join();
 }
 
 #endif
---
+++ src/thread/semaphore.h
@@ -0,0 +1,52 @@
+// Forward declarations
+
+namespace thread
+{
+    class semaphore;
+};
+
+#if !defined(__forward__) && !defined(__drop__thread__semaphore__h)
+#define __drop__thread__semaphore__h
+
+// Libraries
+
+#include <stddef.h>
+
+#ifdef __APPLE__
+#include <dispatch/dispatch.h>
+#endif
+
+// Includes
+
+#include "chrono/time.h"
+
+namespace thread
+{
+    class semaphore
+    {
+        // Members
+
+        #ifdef __APPLE__
+        dispatch_semaphore_t _semaphore;
+        #endif
+
+        size_t _base;
+
+    public:
+
+        // Constructors
+
+        semaphore(const size_t & = 0);
+
+        // Destructor
+
+        ~semaphore();
+
+        // Methods
+
+        bool wait(const microtimestamp & = 0);
+        void post();
+    };
+};
+
+#endif
