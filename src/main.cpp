f91ef7a8830050b04a8c2014a769b6caf248b098
Matteo Monti <matteo.monti@rain.vg>
2017-08-23 13:55:11 +0200

    Main cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,9 @@
 #ifdef __main__
 
 #include <iostream>
-#include <unistd.h>
-#include <thread>
-
-#include "network/pool/pool.hpp"
-#include "async/async.h"
-#include "chrono/crontab.h"
-#include "network/dispatcher.hpp"
-
-network :: pool pool;
-
-class protocol
-{
-    // Self
-
-    typedef protocol self;
-
-public:
-
-    // Protocol
-
-    $packet(packet, int);
-    $packet(other_packet, int, double);
-};
 
 int main()
-{
-    network :: sockets :: udp socket;
-    socket.bind(1234);
-    network :: dispatcher <protocol> sync_dispatcher(socket);
-
-    network :: pool :: dispatcher <protocol> dispatcher = pool.bind(sync_dispatcher);
-
-    dispatcher.receive <protocol :: packet> ().then([](const protocol :: packet & packet)
-    {
-        std :: cout << "Packet received with value " << packet.message() << std :: endl;
-    });
-
-    while(true)
-        usleep(1.e6);
+{    
 }
 
 #endif
