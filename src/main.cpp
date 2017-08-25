7bae850f0616e7c384e01d7e6529d4850d7c2b1b
Matteo Monti <matteo.monti@rain.vg>
2017-08-25 17:12:22 +0200

    Real-case scenario

--- src/main.cpp
+++ src/main.cpp
@@ -6,61 +6,34 @@
 #include "network/dns/message.h"
 #include "network/dns/query.hpp"
 
-int char2int(char input)
-{
-    if(input >= '0' && input <= '9')
-        return input - '0';
-    if(input >= 'A' && input <= 'F')
-        return input - 'A' + 10;
-    if(input >= 'a' && input <= 'f')
-        return input - 'a' + 10;
-}
-
-size_t hex2bin(char* target, const char* src)
-{
-    size_t size = 0;
-
-    while(*src && src[1])
-    {
-        if(*src == ' ')
-        {
-            src++;
-            continue;
-        }
-
-        *(target++) = char2int(*src)*16 + char2int(src[1]);
-        src += 2;
-        size++;
-    }
-
-    return size;
-}
-
 int main()
 {
-    const char hex[] =
-   "50 7d 01 00 00 06 00 00 00 00 00 00 \
-    06 67 6f 6f 67 6c 65 02 69 74 00 00 01 00 01\
-    03 73 75 62 c0 0c 00 01 00 01\
-    03 73 75 62 c0 1b 00 01 00 01\
-    c0 25 00 01 00 01\
-    c0 2f 00 01 00 01\
-    03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 03 61 73 64 00 00 01 00 01";
+    network :: sockets :: udp socket;
+    socket.bind(53);
 
-    char message[512];
-    size_t size = hex2bin(message, hex);
-
-    size_t total = 0;
-    for(size_t n = 0;; n++)
+    while(true)
     {
-        if(n % 1000000 == 0)
-            std :: cout << n << "  (" << total << ")"<< std :: endl;
+        network :: sockets :: udp :: packet packet = socket.receive();
+        network :: dns :: message message(packet.message(), packet.size());
 
-        message[0] = rand();
-        message[1] = rand();
+        std :: cout  << "Id: " << message.id() << std :: endl;
+        std :: cout << "Type: " << message.type() << std :: endl;
+        std :: cout << "Opcode: " << message.opcode() << std :: endl;
+        std :: cout << "Authoritative: " << message.authoritative() << std :: endl;
+        std :: cout << "Truncated: " << message.truncated() << std :: endl;
+        std :: cout << "Rescode: " << message.rescode() << std :: endl << std :: endl;
 
-        network :: dns :: message dnsmessage(message, size);
-        total += dnsmessage.queries.size();
+        std :: cout << "Queries:" << std :: endl << std :: endl;
+        for(size_t i = 0; i < message.queries.size(); i++)
+        {
+            message.queries[i].visit([&](const network :: dns :: query <network :: dns :: A> & query)
+            {
+                std :: cout << " " << i << ": " <<  query.name() << std :: endl;
+            }, [&](const network :: dns :: query <network :: dns :: null> & query)
+            {
+                std :: cout << " " << i << ": " << "Error" << std :: endl;
+            });
+        }
     }
 }
 
